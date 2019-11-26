73594036938562bd9e7aed7e7d6110d27eb28700
AndyJ <andrew.johncey@outlook.com>
2019-11-26 17:07:28 +1100

    Add color and format input fields

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,75 @@
-const now = moment().startOf('hour');
+const now = moment();
 const currentHour = now.hour();
-$("#currentDay").text(now.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+$("#currentDay").text(now.format("ddd, MMM Do YYYY, h:mm A"));
 
 
 const fromTime = now.clone().hour(9);
-const toTime = now.clone().hour(24);
+const toTime = now.clone().hour(22);
 
-console.log(fromTime.format());  
+console.log(fromTime.format());
 console.log(toTime.format());
 
-for (let currentTime = fromTime.clone(); currentTime.isSameOrBefore(toTime); currentTime.add(1,'h'))    {
-    // cloning the clone item
-      console.log(currentTime.format());
+let planner;
+const LOCAL_STORAGE_KEY = 'planner';
 
-    const hourSection = $("#cloneItem").clone();
+const localStoragePlanner = localStorage.getItem(LOCAL_STORAGE_KEY);
+if (localStoragePlanner !== null) {
+      planner = JSON.parse(localStoragePlanner);
+} else {
+      planner = {};
+}
 
-    //   modify the cloned hour section
-    hourSection .removeAttr('id');
-    hourSection.data(".hour", currentTime.hour())
-    hourSection.find(".hour").text(currentTime.format('hA'));
+for (let currentTime = fromTime.clone(); currentTime.isSameOrBefore(toTime); currentTime.add(1, 'h')) {
+  // cloning the clone item
+  console.log(currentTime.format());
 
-$(".plannerContainer").append(hourSection);
+  const hour = currentTime.hour();
+
+  // Cloning the clone item
+  const hourSection = $("#cloneItem").clone();
+
+  // Modify the cloned hour section
+  if (hour < currentHour) {
+    hourSection.addClass('past');
+  }
+  if (hour === currentHour) {
+    hourSection.addClass('present');
+  }
+  if (hour > currentHour) {
+    hourSection.addClass('future');
+  }
+
+  //   modify the cloned hour section
+  hourSection.removeAttr('id');
+  hourSection.data(".hour", hour);
+  hourSection.find(".hour").text(currentTime.format('hA'));
+
+  if (planner[hour] !== undefined) {
+    hourSection.find(".hour")(".memoContent").val(planner[hour]);
+  }
+
+  $(".plannerContainer").append(hourSection);
 };
+
+$("#clearPlannerButton").click(function () {
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  location.reload();
+});
+
+$(".plannerContainer").on("click", "#saveButton", ".saveBtn", function () {
+  const $parent = $(this).parent();
+  const hour = $parent.data('hour');
+  const userInput = $parent.find(".memoContent").val();
+  planner[hour] = userInput;
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(planner));
+});
+
+
+// $(".dayButtons").on("click", "#prevDayButton","#nextDayButton", function () {
+//   const $parent = $(this).parent();
+//   const hour = $parent.data('hour');
+//   const userInput = $parent.find(".memoContent").val();
+//   planner[day] = userInput;
+//   localStorage.getItem(LOCAL_STORAGE_KEY, JSON.stringify(planner));
+// });
+
