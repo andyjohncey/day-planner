dea67a36dca5cc44e21b76faf4d0a7ae5ff527fb
AndyJ <andrew.johncey@outlook.com>
2019-12-12 21:43:51 +1100

    fix clear button

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # day-planner
+Create a online daily planner that can be saved to local storage.
+
+## User Story
+
+AS AN employee with a busy schedule
+
+I WANT to add important events to a daily planner
+
+SO THAT I can manage my time effectively 
--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,48 @@
 const now = moment();
 const currentHour = now.hour();
-$("#currentDay").text(now.format("ddd, MMM Do YYYY, h:mm A"));
+// $("#currentDay").text(now.format("dddd, MMMM Do YYYY, h:mm A"));
+
+function updateTime() {
+  $("#currentDay").text(moment().format("dddd, MMMM Do h:mm:ss a"));
+};
+updateTime();
+setInterval(updateTime, 1000);
 
 
-const fromTime = now.clone().hour(9);
-const toTime = now.clone().hour(22);
+const fromTime = now.clone().hour(7);
+const toTime = now.clone().hour(24);
 
 console.log(fromTime.format());
 console.log(toTime.format());
 
-let planner;
-const LOCAL_STORAGE_KEY = 'planner';
+// const submit = document.querySelector('.hourSection-submit');
+// const memoList = document.querySelector('.memos');
+// const memoInput =('$.memo-input');
+const submit = $('.hoursection-submit')
+const memoInput = $('.memoContent').val();
+const memo = $('.memoContent')
+let planner = {};
 
-const localStoragePlanner = localStorage.getItem(LOCAL_STORAGE_KEY);
-if (localStoragePlanner !== null) {
-      planner = JSON.parse(localStoragePlanner);
-} else {
-      planner = {};
+
+$(".plannerContainer").on("click", ".saveBtn", function () {
+  const text = $(this).parents('.hourSection').find('.memoContent').val();
+  const hour = $(this).parents('.hourSection').data('hour');
+  planner[hour] = text;
+
+  localStorage.setItem('memo', JSON.stringify(planner));
+});
+
+
+const savedString = localStorage.getItem('memo');
+const save = JSON.parse(savedString);
+
+if (save !== null) {
+  planner = save;
 }
 
 for (let currentTime = fromTime.clone(); currentTime.isSameOrBefore(toTime); currentTime.add(1, 'h')) {
   // cloning the clone item
-  console.log(currentTime.format());
+
 
   const hour = currentTime.hour();
 
@@ -41,35 +62,33 @@ for (let currentTime = fromTime.clone(); currentTime.isSameOrBefore(toTime); cur
 
   //   modify the cloned hour section
   hourSection.removeAttr('id');
-  hourSection.data(".hour", hour);
-  hourSection.find(".hour").text(currentTime.format('hA'));
+  hourSection.data("hour", hour);
 
+  hourSection.find(".hour").text(currentTime.format('hA'));
   if (planner[hour] !== undefined) {
-    hourSection.find(".hour")(".memoContent").val(planner[hour]);
+    hourSection.find('.memoContent').val(planner[hour]);
   }
 
   $(".plannerContainer").append(hourSection);
 };
 
-$("#clearPlannerButton").click(function () {
-  localStorage.removeItem(LOCAL_STORAGE_KEY);
+$("#clearPlannerBtn").on("click", function () {
+  localStorage.clear(memo);
   location.reload();
 });
 
-$(".plannerContainer").on("click", "#saveButton", ".saveBtn", function () {
-  const $parent = $(this).parent();
-  const hour = $parent.data('hour');
-  const userInput = $parent.find(".memoContent").val();
-  planner[hour] = userInput;
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(planner));
-});
+$("#cloneItem").click((function () {
+  event.preventDefault();
+  const target = $(event.target);
+  if (target.hasClass('hour')) {
+    target.removeClass('hour');
+    target.addClass('hours')
+    console.log('saving memo');
 
+    const memo = $(this).find(".memoContent").text();
+    const localStorage = localStorage.getItem('memo');
+    const storage = localStorage('memo', JSON.parse(".memoContent").innerHTML);
 
-// $(".dayButtons").on("click", "#prevDayButton","#nextDayButton", function () {
-//   const $parent = $(this).parent();
-//   const hour = $parent.data('hour');
-//   const userInput = $parent.find(".memoContent").val();
-//   planner[day] = userInput;
-//   localStorage.getItem(LOCAL_STORAGE_KEY, JSON.stringify(planner));
-// });
-
+    localStorage.setItem('memo', JSON.stringify('.memoContent').innerHTML);
+  }
+}));
